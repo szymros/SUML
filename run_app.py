048de614abdf5eff399f82319a5d928e4a8307ee
s14324 <62269287+s14324@users.noreply.github.com>
2024-01-29 10:47:55 +0100

    Add pyinstaller handling
    Updated README.md and requirements.txt accordingly

---
+++ run_app.py
@@ -0,0 +1,20 @@
+import os
+import streamlit.web.bootstrap
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+
+    flag_options = {
+        "server.port": 8501,
+        "global.developmentMode": False,
+    }
+
+    streamlit.web.bootstrap.load_config_options(flag_options=flag_options)
+    flag_options["_is_running_with_streamlit"] = True
+    streamlit.web.bootstrap.run(
+        "./app.py",
+        "streamlit run",
+        [],
+        flag_options,
+    )
+    
